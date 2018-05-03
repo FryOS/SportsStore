c4e7fdc1c0785b285d2d5834c1e5ef5f921154b0
Alexey <alexeyosp@gmail.com>
2018-05-03 23:07:14 +0300

    Страницы перемещения

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,7 +9,9 @@ import { ProductRepository } from '../model/product.repository';
 })
 export class StoreComponent implements OnInit {
 
-  public selectCategory = null;
+  public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(
     private repository: ProductRepository) { }
@@ -18,14 +20,26 @@ export class StoreComponent implements OnInit {
   }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
-
   get categories(): string[] {
     return this.repository.getCategories();
   }
-
   changeCategory(newCategory?: string) {
-  this.selectCategory = newCategory;
+    this.selectedCategory = newCategory;
+  }
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
   }
 }
