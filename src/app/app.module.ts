6cfdcb5883316ee3c0d0810b84a1cd665c42ed65
FryOS <alexeyosp@gmail.com>
2018-05-04 14:13:20 +0300

    Настроил роуты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-// import { StoreComponent } from './store/store.component';
+import { StoreComponent } from './store/store.component';
 import { StoreModule } from './store/store.module';
-
+import { CheckoutComponent } from './store/checkout/checkout.component';
+import { CartDetailComponent } from './store/cart-detail/cart-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    // StoreComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    StoreModule
-  ],
-  providers: [],
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: 'store', component: StoreComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: '/store' }
+    ])],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
