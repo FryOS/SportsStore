01d7e70477c5540f49d44b519f87397e42e6950d
FryOS <alexeyosp@gmail.com>
2018-05-04 15:43:38 +0300

    с добавлением storeFirst.guard.ts изменили и роуты(пути)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,23 @@ import { StoreModule } from './store/store.module';
 import { CheckoutComponent } from './store/checkout/checkout.component';
 import { CartDetailComponent } from './store/cart-detail/cart-detail.component';
 import { RouterModule } from '@angular/router';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 @NgModule({
   imports: [BrowserModule, StoreModule,
     RouterModule.forRoot([
-      { path: 'store', component: StoreComponent },
-      { path: 'cart', component: CartDetailComponent },
-      { path: 'checkout', component: CheckoutComponent },
+      { path: 'store', component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path: 'cart', component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path: 'checkout', component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
       { path: '**', redirectTo: '/store' }
     ])],
+  providers: [StoreFirstGuard],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
